cdc5fe049136640aef6f2f4dcad5d04f8a6ade88
Rick Black <therickblack@gmail.com>
2025-12-03 09:51:11 -0500

    reworked arg parsing to support list of commands

--- AppCommandControls/Program.cs
+++ AppCommandControls/Program.cs
@@ -5,43 +5,31 @@ using System.Runtime.InteropServices;
 static extern int GetForegroundWindow();
 [DllImport("user32.dll")]
 static extern int SendMessage(int hWnd, int Msg, int wParam, int lParam);
-
+List<AppComandCode> AppComandCodeList = new();
 const int WM_APPCOMMAND =   0x319;
 var window = GetForegroundWindow();
-var command =   args[0];
-var appCommand =  AppComandCode.NONE;
-
-switch (command)
+string command = "";
+if(args!=null && args.Length > 0)
 {
-    case "previous":
-        Console.WriteLine($"command:  {command}");
-        appCommand =  AppComandCode.MEDIA_PREVIOUSTRACK;
-        break;
-    case "play/pause":
-        Console.WriteLine($"command:  {command}");
-        appCommand = AppComandCode.MEDIA_PLAY_PAUSE;
-        break;
-    case "next":
-        Console.WriteLine($"command:  {command}");
-        appCommand = AppComandCode.MEDIA_NEXTTRACK;
-        break;
-    case "up":
-        Console.WriteLine($"command:  {command}");
-        appCommand = AppComandCode.VOLUME_UP;
-        break;
-    case "down":
-        Console.WriteLine($"command:  {command}");
-        appCommand = AppComandCode.VOLUME_DOWN;
-        break;
-    case "mute":
-        Console.WriteLine($"command:  {command}");
-        appCommand = AppComandCode.VOLUME_MUTE;
-        break;
-    default:
-        Console.WriteLine($"command not recognized: {command}");
-        break;
+    for(int i =0; i < args.Length; i++)
+    {
+        var appCommand = AppComandCode.NONE;
+        try
+        {
+            appCommand = (AppComandCode)System.Enum.Parse(typeof(AppComandCode), args[i]);
+        }
+        catch (Exception ex)
+        {
+        
+        }
+        if (appCommand != AppComandCode.NONE)
+        {
+            AppComandCodeList.Add(appCommand);
+        }
+    }
 }
-if (appCommand != AppComandCode.NONE)
+foreach(var appCommand in AppComandCodeList)
 {
     SendMessage(window, WM_APPCOMMAND, 0, ((int)appCommand) * 65536);
+    System.Threading.Thread.Sleep(200);
 }
